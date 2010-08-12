924990cfc4ade7cde5fa9b33ac92fab0b59ee6c7
Pierre Minnieur <pm@pierre-minnieur.de>
2010-08-13 00:15:32 +0200

    replaced tabs with spaces

--- entropy.js
+++ entropy.js
@@ -1,8 +1,8 @@
 var fs  = require("fs"),
-	sys = require("sys");
+    sys = require("sys");
 
 var express = require("express"),
-	connect = require("connect");
+    connect = require("connect");
 
 var entropy = express.createServer();
 
@@ -10,49 +10,49 @@ entropy.use(connect.conditionalGet());
 entropy.use(connect.methodOverride());
 
 try {
-	configJSON = fs.readFileSync(__dirname+"/config.json");
+    configJSON = fs.readFileSync(__dirname+"/config.json");
 } catch(e) {
-	sys.log("File config.json not found. Try: `cp config.json.sample config.json`");
+    sys.log("File config.json not found. Try: `cp config.json.sample config.json`");
 }
 var config = JSON.parse(configJSON.toString());
 
 if (config.debug) {
-	entropy.use(connect.errorHandler({
-		showStack: true,
-		dumpExceptions: true
-	}));
+    entropy.use(connect.errorHandler({
+        showStack: true,
+        dumpExceptions: true
+    }));
 }
 
 if (config.logger) {
-	entropy.use(connect.logger());
+    entropy.use(connect.logger());
 }
 
 var mongoose = require("mongoose").Mongoose,
-	mongodb  = mongoose.connect("mongodb://"+config.mongo.host+":"+config.mongo.port+"/"+config.mongo.name);
+    mongodb  = mongoose.connect("mongodb://"+config.mongo.host+":"+config.mongo.port+"/"+config.mongo.name);
 
 // FIND
 entropy.get("/:collection", function(req, res) {
-	res.send("find:"+req.param("collection"), 200);
+    res.send("find:"+req.param("collection"), 200);
 });
 
 // READ
 entropy.get("/:collection/:id", function(req, res) {
-	res.send("read:"+req.param("collection")+"/"+req.param("id"), 200);
+    res.send("read:"+req.param("collection")+"/"+req.param("id"), 200);
 });
 
 // CREATE
 entropy.post("/:collection", function(req, res) {
-	res.send("create:"+req.param("collection"), 201);
+    res.send("create:"+req.param("collection"), 201);
 });
 
 // MODIFY
 entropy.post("/:collection/:id", function(req, res) {
-	res.send("modify:"+req.param("collection")+"/"+req.param("id"), 200);
+    res.send("modify:"+req.param("collection")+"/"+req.param("id"), 200);
 });
 
 // REMOVE
 entropy.del("/:collection/:id", function(req, res) {
-	res.send("remove:"+req.param("collection")+"/"+req.param("id"), 200);
+    res.send("remove:"+req.param("collection")+"/"+req.param("id"), 200);
 });
 
 entropy.listen(config.server.port, config.server.addr);
